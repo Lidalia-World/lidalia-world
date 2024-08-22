bb8e275477df7577a6bd72bf0d3b0ffb10a8346e
Robert Elliot <rob@lidalia.org.uk>
2024-08-22 10:54:22 +0100

    Quieten warnings

--- encoding/core/src/main/kotlin/Encodable.kt
+++ encoding/core/src/main/kotlin/Encodable.kt
@@ -1,7 +1,6 @@
 interface Encodable<Self : Encodable<Self>> {
+  @Suppress("UNCHECKED_CAST")
   fun <RawEncoded, E : Encoded<Self, RawEncoded>> encode(
     encoder: Encoder<Self, RawEncoded, E>,
-  ): E {
-    return encoder.encode(this as Self)
-  }
+  ): E = encoder.encode(this as Self)
 }
